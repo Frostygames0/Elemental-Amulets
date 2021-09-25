fe65db82f89c494571e663a97e1a04f77b441161
Frostygames0 <vanya.alekseev6@gmail.com>
2021-09-25 17:25:04 +0300

    Fixed server side crash

--- src/main/java/frostygames0/elementalamulets/client/ModKeyBindings.java
+++ src/main/java/frostygames0/elementalamulets/client/ModKeyBindings.java
@@ -5,6 +5,7 @@ import frostygames0.elementalamulets.network.COpenAmuletBeltGUIPacket;
 import frostygames0.elementalamulets.network.ModNetworking;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.client.util.InputMappings;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -15,7 +16,7 @@ import net.minecraftforge.fml.common.Mod;
  * @author Frostygames0
  * @date 21.09.2021 19:32
  */
-@Mod.EventBusSubscriber(modid = ElementalAmulets.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+@Mod.EventBusSubscriber(modid = ElementalAmulets.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ModKeyBindings {
     public static KeyBinding OPEN_AMULET_BELT = new KeyBinding("key.elementalamulets.open_belt_gui", KeyConflictContext.IN_GAME, InputMappings.UNKNOWN, "key.categories.inventory");
 
