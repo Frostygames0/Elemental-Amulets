bbbe887281c536e6da28e1fd05bfb195af4b7b57
Frostygames0 <vanya.alekseev6@gmail.com>
2021-09-27 20:31:07 +0300

    Now Elemental Focus will open combinator under them! Multiblock :)

--- src/main/java/frostygames0/elementalamulets/blocks/ElementalFocus.java
+++ src/main/java/frostygames0/elementalamulets/blocks/ElementalFocus.java
@@ -1,14 +1,23 @@
 package frostygames0.elementalamulets.blocks;
 
+import frostygames0.elementalamulets.blocks.tiles.ElementalCombinatorTile;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.network.NetworkHooks;
 
 
 import javax.annotation.Nullable;
@@ -32,5 +41,22 @@ public class ElementalFocus extends Block {
     @Override
     public boolean propagatesSkylightDown(BlockState p_200123_1_, IBlockReader p_200123_2_, BlockPos p_200123_3_) {
         return true;
+    }
+
+    @Override
+    public ActionResultType use(BlockState pState, World pLevel, BlockPos pPos, PlayerEntity pPlayer, Hand pHand, BlockRayTraceResult pHit) {
+        if(pLevel.isClientSide()) {
+            return ActionResultType.SUCCESS;
+        }
+        TileEntity te = pLevel.getBlockEntity(pPos.below());
+        if(te instanceof ElementalCombinatorTile) {
+            ElementalCombinatorTile elementalCombinatorTile = (ElementalCombinatorTile) te;
+            if(!pPlayer.isShiftKeyDown()) {
+                NetworkHooks.openGui((ServerPlayerEntity) pPlayer, elementalCombinatorTile, elementalCombinatorTile.getBlockPos());
+            } else {
+                elementalCombinatorTile.startCombination();
+            }
+        }
+        return ActionResultType.CONSUME;
     }
 }
