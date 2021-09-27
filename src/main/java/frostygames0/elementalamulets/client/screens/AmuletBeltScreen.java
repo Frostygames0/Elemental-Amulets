761e39e724eec66b2e889b8d4ae6e8026f0b4bf6
Frostygames0 <vanya.alekseev6@gmail.com>
2021-09-27 20:38:14 +0300

    Moved label a little up

--- src/main/java/frostygames0/elementalamulets/client/screens/AmuletBeltScreen.java
+++ src/main/java/frostygames0/elementalamulets/client/screens/AmuletBeltScreen.java
@@ -19,7 +19,7 @@ public class AmuletBeltScreen extends ContainerScreen<AmuletBeltContainer> {
     public static final ResourceLocation GUI = modPrefix("textures/gui/amulet_belt_gui.png");
     public AmuletBeltScreen(AmuletBeltContainer screenContainer, PlayerInventory inv, ITextComponent titleIn) {
         super(screenContainer, inv, titleIn);
-        this.titleLabelY = this.topPos+46;
+        this.titleLabelY = this.topPos+43;
     }
 
     @Override
