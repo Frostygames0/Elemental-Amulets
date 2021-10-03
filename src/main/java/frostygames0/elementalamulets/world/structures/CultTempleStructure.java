78a53fe5e99295ec9db74a6c049e10070885f687
Frostygames0 <vanya.alekseev6@gmail.com>
2021-10-03 21:03:51 +0300

    Now temple checks if there is water underneath it, so it wont spawn it underwater. Also now it will spawn up to 5 creepers inside. and i removed moving it two blocks down

--- src/main/java/frostygames0/elementalamulets/world/structures/CultTempleStructure.java
+++ src/main/java/frostygames0/elementalamulets/world/structures/CultTempleStructure.java
@@ -1,19 +1,31 @@
 package frostygames0.elementalamulets.world.structures;
 
+import com.google.common.collect.ImmutableList;
 import frostygames0.elementalamulets.ElementalAmulets;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.EntityType;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SharedSeedRandom;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MutableBoundingBox;
 import net.minecraft.util.math.vector.Vector3i;
 import net.minecraft.util.registry.DynamicRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.MobSpawnInfo;
+import net.minecraft.world.biome.provider.BiomeProvider;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.jigsaw.JigsawManager;
 import net.minecraft.world.gen.feature.structure.*;
 import net.minecraft.world.gen.feature.template.TemplateManager;
+
+
+import java.util.List;
 
 
 /**
@@ -35,6 +47,23 @@ public class CultTempleStructure extends Structure<NoFeatureConfig> {
         return GenerationStage.Decoration.SURFACE_STRUCTURES;
     }
 
+    @Override
+    public List<MobSpawnInfo.Spawners> getDefaultSpawnList() {
+        return ImmutableList.of(new MobSpawnInfo.Spawners(EntityType.CREEPER, 100, 2, 5));
+    }
+
+    @Override
+    protected boolean isFeatureChunk(ChunkGenerator chunkGenerator, BiomeProvider biomeSource, long seed, SharedSeedRandom chunkRandom, int chunkX, int chunkZ, Biome biome, ChunkPos chunkPos, NoFeatureConfig featureConfig) {
+        BlockPos centerOfChunk = new BlockPos(chunkX * 16, 0, chunkZ * 16);
+
+        int landHeight = chunkGenerator.getFirstOccupiedHeight(centerOfChunk.getX(), centerOfChunk.getZ(), Heightmap.Type.WORLD_SURFACE_WG);
+
+        IBlockReader columnOfBlocks = chunkGenerator.getBaseColumn(centerOfChunk.getX(), centerOfChunk.getZ());
+        BlockState topBlock = columnOfBlocks.getBlockState(centerOfChunk.above(landHeight));
+
+        return topBlock.getFluidState().isEmpty() && landHeight <= 65; // This should prevent spawning it on water
+    }
+
     public static class Start extends StructureStart<NoFeatureConfig> {
 
         public Start(Structure<NoFeatureConfig> structureIn, int chunkX, int chunkZ, MutableBoundingBox mutableBoundingBox, int referenceIn, long seedIn) {
@@ -46,11 +75,7 @@ public class CultTempleStructure extends Structure<NoFeatureConfig> {
         public void generatePieces(DynamicRegistries dynamicRegistryManager, ChunkGenerator chunkGenerator, TemplateManager templateManagerIn, int chunkX, int chunkZ, Biome biomeIn, NoFeatureConfig config) {
             BlockPos centerPos = new BlockPos(chunkX * 16, 0, chunkZ * 16);
 
-            JigsawManager.addPieces(dynamicRegistryManager, new VillageConfig(() -> dynamicRegistryManager.registryOrThrow(Registry.TEMPLATE_POOL_REGISTRY).get(new ResourceLocation(ElementalAmulets.MOD_ID, "cult_temple/cult_temple_start")), 5), AbstractVillagePiece::new, chunkGenerator, templateManagerIn, centerPos, pieces, random, false,true);
-
-            this.pieces.forEach(piece -> piece.move(0, -2, 0));
-            this.pieces.forEach(piece -> piece.getBoundingBox().y0 -= 2);
-
+            JigsawManager.addPieces(dynamicRegistryManager, new VillageConfig(() -> dynamicRegistryManager.registryOrThrow(Registry.TEMPLATE_POOL_REGISTRY).get(new ResourceLocation(ElementalAmulets.MOD_ID, "cult_temple/cult_temple_entrance")), 10), AbstractVillagePiece::new, chunkGenerator, templateManagerIn, centerPos, pieces, random, false,true);
 
             Vector3i structureCenter = this.pieces.get(0).getBoundingBox().getCenter();
             int xOffset = centerPos.getX() - structureCenter.getX();
