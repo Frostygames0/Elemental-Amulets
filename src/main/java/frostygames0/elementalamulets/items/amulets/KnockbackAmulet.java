8cfe5e36bce478e50e34c2757a0ec8cb4141f0bf
Frostygames0 <vanya.alekseev6@gmail.com>
2021-10-09 12:43:50 +0300

    Stub methods for upcoming amulet

---
+++ src/main/java/frostygames0/elementalamulets/items/amulets/KnockbackAmulet.java
@@ -0,0 +1,28 @@
+/*
+ *    This file is part of Elemental Amulets.
+ *
+ *     Elemental Amulets is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Elemental Amulets is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Elemental Amulets.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package frostygames0.elementalamulets.items.amulets;
+
+/**
+ * @author Frostygames0
+ * @date 09.10.2021 11:44
+ */
+public class KnockbackAmulet extends AmuletItem{
+    public KnockbackAmulet(Properties properties) {
+        super(properties);
+    }
+}
