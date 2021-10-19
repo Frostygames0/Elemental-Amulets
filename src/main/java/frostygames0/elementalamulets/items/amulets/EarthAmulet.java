16a57b5e5614e654f7b13f578ff7b190a1550e1c
Frostygames0 <vanya.alekseev6@gmail.com>
2021-10-19 22:29:43 +0300

    Changed algorithm of Earth Amulet

--- src/main/java/frostygames0/elementalamulets/items/amulets/EarthAmulet.java
+++ src/main/java/frostygames0/elementalamulets/items/amulets/EarthAmulet.java
@@ -20,16 +20,14 @@
 package frostygames0.elementalamulets.items.amulets;
 
 import frostygames0.elementalamulets.ElementalAmulets;
+import frostygames0.elementalamulets.util.NBTUtil;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.GrassBlock;
 import net.minecraft.block.IGrowable;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.vector.Vector3i;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
@@ -44,7 +42,9 @@ import java.util.Random;
  * @author Frostygames0
  * @date 19.09.2021 22:50
  */
+// TODO: Add more effects, maybe saturation when in forest idk
 public class EarthAmulet extends AmuletItem{
+    private static final String COOLDOWN_TAG = ElementalAmulets.MOD_ID+":cooldown";
     public EarthAmulet(Properties properties) {
         super(properties);
     }
@@ -61,22 +61,36 @@ public class EarthAmulet extends AmuletItem{
         super.curioTick(identifier, index, livingEntity, stack);
         World world = livingEntity.level;
         if(!world.isClientSide()) {
-            BlockPos playerPos = livingEntity.blockPosition();
-            BlockPos.betweenClosedStream(new AxisAlignedBB(playerPos.subtract(new Vector3i(4, 1, 4)), playerPos.offset(new Vector3i(4, 3, 4)))).forEach(blockPos -> {
-                BlockState blockState = world.getBlockState(blockPos);
-                Block block = blockState.getBlock();
-                if(block instanceof IGrowable && !(block instanceof GrassBlock)) {
-                    if(livingEntity.tickCount % 60 == 0) {
-                        Random random = livingEntity.getRandom();
-                        if (random.nextInt() >= this.getTier(stack) * 5 * 1.5) {
-                            if(((IGrowable)block).isValidBonemealTarget(world, blockPos, blockState, false)) {
-                                ((IGrowable)block).performBonemeal((ServerWorld) world, random, blockPos, blockState);
-                                ElementalAmulets.LOGGER.debug("Applied Earth Amulet effect to "+block+" at "+ blockPos);
-                            }
-                        }
+            int cooldown = NBTUtil.getInteger(stack, COOLDOWN_TAG);
+            if(cooldown < 1) {
+                boostLocalPlants(world, stack, livingEntity.blockPosition(), livingEntity.getRandom());
+            }
+            if(cooldown > 0) NBTUtil.putInteger(stack, COOLDOWN_TAG, cooldown-1);
+            ElementalAmulets.LOGGER.debug(cooldown);
+        }
+    }
+
+    // TODO: Make it grow plants randomly and not in order
+    private void boostLocalPlants(World world, ItemStack amulet, BlockPos centerPos, Random random) {
+        for (BlockPos blockPos : BlockPos.betweenClosed(centerPos.getX() - 4, centerPos.getY() - 1, centerPos.getZ() - 4,
+                centerPos.getX() + 4, centerPos.getY() + 3, centerPos.getZ() + 4)) {
+
+            BlockState blockState = world.getBlockState(blockPos);
+            Block block = blockState.getBlock();
+
+            if (block instanceof IGrowable) {
+                IGrowable growable = (IGrowable) block;
+                if (random.nextInt(20) <= this.getTier(amulet)) {
+                    if (growable.isValidBonemealTarget(world, blockPos, blockState, false)) {
+                        growable.performBonemeal((ServerWorld) world, random, blockPos, blockState);
+
+                        ElementalAmulets.LOGGER.debug("Boosted " + block.getName() + " at " + blockPos.toShortString());
+
+                        NBTUtil.putInteger(amulet, COOLDOWN_TAG, 100);
+                        break;
                     }
                 }
-            });
+            }
         }
     }
 }
