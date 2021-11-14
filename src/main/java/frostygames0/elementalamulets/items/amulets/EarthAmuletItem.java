3d253ee8bf489e3c818b7e39c33dc08fdabb5453
Ваня <vanya.alekseev6@gmail.com>
2021-11-15 01:10:56 +0300

    Added regeneration ability prototype

--- src/main/java/frostygames0/elementalamulets/items/amulets/EarthAmuletItem.java
+++ src/main/java/frostygames0/elementalamulets/items/amulets/EarthAmuletItem.java
@@ -27,9 +27,14 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.CooldownTracker;
+import net.minecraft.util.RegistryKey;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.BiomeDictionary;
 
 
 import java.util.Random;
@@ -53,8 +58,8 @@ public class EarthAmuletItem extends AmuletItem{
                 if (!cooldownTracker.isOnCooldown(this)) {
                     int boosted = boostLocalPlants(world, stack, player.blockPosition(), random);
                     if(boosted > 0) {
+                        this.regenerate(livingEntity, stack, boosted);
                         cooldownTracker.addCooldown(this, ModConfig.CachedValues.EARTH_AMULET_COOLDOWN);
-
                     }
                 }
             }
@@ -80,5 +85,20 @@ public class EarthAmuletItem extends AmuletItem{
             }
         }
         return boosted;
+    }
+
+    // TODO still needs some work
+    private void regenerate(LivingEntity living, ItemStack stack, int boosted) {
+        World level = living.level;
+        if(getTier(stack) > 1) {
+            RegistryKey<Biome> biome = level.getBiomeName(living.blockPosition()).orElseThrow(() -> new NullPointerException("Cannot identify a biome!"));
+            if(BiomeDictionary.hasType(biome, BiomeDictionary.Type.PLAINS) || BiomeDictionary.hasType(biome, BiomeDictionary.Type.FOREST)) {
+                living.heal(0.1f * boosted);
+
+                if(living instanceof PlayerEntity) {
+                    ((PlayerEntity)living).displayClientMessage(new StringTextComponent("You feel hehe penis").withStyle(TextFormatting.GREEN), true);
+                }
+            }
+        }
     }
 }
