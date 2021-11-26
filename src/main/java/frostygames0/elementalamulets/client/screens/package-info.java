3ce5b9d74d0becfa46364c4b40b8e291e888fb64
Frostygames0 <39121739+frostygames0@users.noreply.github.com>
2021-11-26 18:49:43 +0300

    Added package-info.java to almost all packages in this project, so it won't tell me that: PARAMETERSARENONNULLBYDEFAULT
    Signed-off-by: Ваня <vanya.alekseev6@gmail.com>

---
+++ src/main/java/frostygames0/elementalamulets/client/screens/package-info.java
@@ -0,0 +1,27 @@
+/*
+ *     Copyright (c) 2021
+ *
+ *     This file is part of Elemental Amulets, a Minecraft Mod.
+ *
+ *     Elemental Amulets is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Elemental Amulets is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Elemental Amulets.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @author Frostygames0
+ * @date 26.11.2021 18:42
+ */
+@ParametersAreNonnullByDefault
+package frostygames0.elementalamulets.client.screens;
+
+import javax.annotation.ParametersAreNonnullByDefault;
