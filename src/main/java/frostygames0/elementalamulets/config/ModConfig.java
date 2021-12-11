612ea6d0d50b07d4bbf6f9635f1802bb4a640712
Ваня <vanya.alekseev6@gmail.com>
2021-12-11 13:36:03 +0300

    Reformatted config fields
    Signed-off-by: Ваня <vanya.alekseev6@gmail.com>

--- src/main/java/frostygames0/elementalamulets/config/ModConfig.java
+++ src/main/java/frostygames0/elementalamulets/config/ModConfig.java
@@ -40,30 +40,30 @@ public class ModConfig {
     }
 
     public static class Server {
-        private final ForgeConfigSpec.BooleanValue FANCY_COMBINATION;
-        private final ForgeConfigSpec.BooleanValue OLD_FASHIONED_WAY;
+        private final ForgeConfigSpec.BooleanValue fancyCombination;
+        private final ForgeConfigSpec.BooleanValue oldFashionedWay;
 
-        private final ForgeConfigSpec.DoubleValue JUMP_AMULET_BOOST;
-        private final ForgeConfigSpec.DoubleValue FIRE_AMULET_FIRE_RESISTANCE;
-        private final ForgeConfigSpec.DoubleValue FIRE_AMULET_LAVA_RESISTANCE;
-        private final ForgeConfigSpec.DoubleValue SPEED_AMULET_BOOST;
-        private final ForgeConfigSpec.DoubleValue PROTECTION_AMULET_REFLECT_DAMAGE_MULT;
-        private final ForgeConfigSpec.IntValue PROTECTION_AMULET_CHARGE_TIME;
-        private final ForgeConfigSpec.DoubleValue WATER_AMULET_SPEED_BOOST;
-        private final ForgeConfigSpec.IntValue EARTH_AMULET_COOLDOWN;
-        private final ForgeConfigSpec.IntValue PACIFYING_AMULET_BREAK_COOLDOWN;
-        private final ForgeConfigSpec.BooleanValue PACIFYING_AMULET_ANGER_ONBREAK;
-        private final ForgeConfigSpec.IntValue PACIFYING_AMULET_DISORIENTATION_TIME;
-        private final ForgeConfigSpec.BooleanValue MODIFY_VANILLA_LOOT;
+        private final ForgeConfigSpec.DoubleValue jumpAmuletBoost;
+        private final ForgeConfigSpec.DoubleValue fireAmuletFireResistance;
+        private final ForgeConfigSpec.DoubleValue fireAmuletLavaResistance;
+        private final ForgeConfigSpec.DoubleValue speedAmuletBoost;
+        private final ForgeConfigSpec.DoubleValue protectionAmuletReflectDamageMult;
+        private final ForgeConfigSpec.IntValue protectionAmuletChargeTime;
+        private final ForgeConfigSpec.DoubleValue waterAmuletSpeedBoost;
+        private final ForgeConfigSpec.IntValue earthAmuletCooldown;
+        private final ForgeConfigSpec.IntValue pacifyingAmuletBreakCooldown;
+        private final ForgeConfigSpec.BooleanValue pacifyingAmuletAngerOnbreak;
+        private final ForgeConfigSpec.IntValue pacifyingAmuletDisorientationTime;
+        private final ForgeConfigSpec.BooleanValue modifyVanillaLoot;
 
         public Server(ForgeConfigSpec.Builder builder) {
             builder.push("Loot");
-            MODIFY_VANILLA_LOOT = builder.comment("Add mod items to the vanilla loot tables [DEFAULT: true]").define("modify_vanilla_loot", true);
+            modifyVanillaLoot = builder.comment("Add mod items to the vanilla loot tables [DEFAULT: true]").define("modify_vanilla_loot", true);
             builder.pop();
 
             builder.push("Elemental Combinator");
-            OLD_FASHIONED_WAY = builder.comment("Allow usage of old combination procedure(Shift-Click on the block) [DEFAULT: false]").define("elemental_combinator_old_way", false);
-            FANCY_COMBINATION = builder.comment("Add special effects when Elemental Combinator is working [DEFAULT: true]").define("elemental_combinator_special_effects", true);
+            oldFashionedWay = builder.comment("Allow usage of old combination procedure(Shift-Click on the block) [DEFAULT: false]").define("elemental_combinator_old_way", false);
+            fancyCombination = builder.comment("Add special effects when Elemental Combinator is working [DEFAULT: true]").define("elemental_combinator_special_effects", true);
             builder.pop();
 
             builder.comment("Anything that is related to the amulets [WARNING! Setting any of these values too high may cause bugs, lags and even crashes]")
@@ -71,35 +71,35 @@ public class ModConfig {
 
 
             builder.push("Amulet of Jump-Boost");
-            JUMP_AMULET_BOOST = builder.comment("How high will players jump with Jump Amulet [DEFAULT: 0.3]").defineInRange("jump_amulet_boost", 0.3, 0, Float.MAX_VALUE);
+            jumpAmuletBoost = builder.comment("How high will players jump with Jump Amulet [DEFAULT: 0.3]").defineInRange("jump_amulet_boost", 0.3, 0, Float.MAX_VALUE);
             builder.pop();
 
             builder.push("Amulet of Fire-Resistance");
-            FIRE_AMULET_FIRE_RESISTANCE = builder.comment("How good will Amulet of Fire-Resistance protect from fire [DEFAULT: 0.5]").defineInRange("fire_amulet_fire_resistance", 0.5, 0, Float.MAX_VALUE);
-            FIRE_AMULET_LAVA_RESISTANCE = builder.comment("How good will Amulet of Fire-Resistance protect from lava [DEFAULT: 0.25]").defineInRange("fire_amulet_lava_resistance", 0.25, 0, Float.MAX_VALUE);
+            fireAmuletFireResistance = builder.comment("How good will Amulet of Fire-Resistance protect from fire [DEFAULT: 0.5]").defineInRange("fire_amulet_fire_resistance", 0.5, 0, Float.MAX_VALUE);
+            fireAmuletLavaResistance = builder.comment("How good will Amulet of Fire-Resistance protect from lava [DEFAULT: 0.25]").defineInRange("fire_amulet_lava_resistance", 0.25, 0, Float.MAX_VALUE);
             builder.pop();
 
             builder.push("Amulet of Speed-Boost");
-            SPEED_AMULET_BOOST = builder.comment("How fast will players run with Amulet of Speed-Boost [DEFAULT: 1.08]").defineInRange("speed_amulet_boost", 1.08, 0, Float.MAX_VALUE);
+            speedAmuletBoost = builder.comment("How fast will players run with Amulet of Speed-Boost [DEFAULT: 1.08]").defineInRange("speed_amulet_boost", 1.08, 0, Float.MAX_VALUE);
             builder.pop();
 
             builder.push("Amulet of Terra-Protection");
-            PROTECTION_AMULET_REFLECT_DAMAGE_MULT = builder.comment("How much damage will Leaf Shield absorb? [DEFAULT: 0.5]").defineInRange("protection_amulet_absorption", 0.5f, 0, Float.MAX_VALUE);
-            PROTECTION_AMULET_CHARGE_TIME = builder.comment("How long will leaf shield recharge one bar (in ticks, 1 sec - 20 ticks)? [DEFAULT: 80]").defineInRange("protection_amulet_recharge_time", 80, 0, Integer.MAX_VALUE);
+            protectionAmuletReflectDamageMult = builder.comment("How much damage will Leaf Shield absorb? [DEFAULT: 0.5]").defineInRange("protection_amulet_absorption", 0.5f, 0, Float.MAX_VALUE);
+            protectionAmuletChargeTime = builder.comment("How long will leaf shield recharge one bar (in ticks, 1 sec - 20 ticks)? [DEFAULT: 80]").defineInRange("protection_amulet_recharge_time", 80, 0, Integer.MAX_VALUE);
             builder.pop();
 
             builder.push("Amulet of Water");
-            WATER_AMULET_SPEED_BOOST = builder.comment("How fast will players swim with Water Amulet [DEFAULT 0.5]").defineInRange("water_amulet_speed_boost", 0.5f, 0, Float.MAX_VALUE);
+            waterAmuletSpeedBoost = builder.comment("How fast will players swim with Water Amulet [DEFAULT 0.5]").defineInRange("water_amulet_speed_boost", 0.5f, 0, Float.MAX_VALUE);
             builder.pop();
 
             builder.push("Amulet of Earth");
-            EARTH_AMULET_COOLDOWN = builder.comment("How long will the Amulet of Earth be on cooldown? (in ticks, 1 sec - 20 ticks) [DEFAULT: 100]").defineInRange("earth_amulet_cooldown", 100, 0, Integer.MAX_VALUE);
+            earthAmuletCooldown = builder.comment("How long will the Amulet of Earth be on cooldown? (in ticks, 1 sec - 20 ticks) [DEFAULT: 100]").defineInRange("earth_amulet_cooldown", 100, 0, Integer.MAX_VALUE);
             builder.pop();
 
             builder.push("Pacifying Amulet");
-            PACIFYING_AMULET_DISORIENTATION_TIME = builder.comment("How long will the enemy  be disoriented (if enemy can't be pacified)? (in ticks, 1 sec - 20 ticks) [DEFAULT: 100]").defineInRange("pacifying_amulet_tiredness_time", 100, 0, Integer.MAX_VALUE);
-            PACIFYING_AMULET_BREAK_COOLDOWN = builder.comment("How long will you need to wait until you can use Pacifying Amulet when broken (in ticks, 1 sec - 20 ticks) [DEFAULT: 200]").defineInRange("pacifying_amulet_break_cooldown", 200, 0, Integer.MAX_VALUE);
-            PACIFYING_AMULET_ANGER_ONBREAK = builder.comment("Anger all nearby 'angerable'(wolves, golems, etc) entities when pacifying amulet breaks? [DEFAULT: true]").define("pacifying_amulet_anger_onbreak", true);
+            pacifyingAmuletDisorientationTime = builder.comment("How long will the enemy  be disoriented (if enemy can't be pacified)? (in ticks, 1 sec - 20 ticks) [DEFAULT: 100]").defineInRange("pacifying_amulet_tiredness_time", 100, 0, Integer.MAX_VALUE);
+            pacifyingAmuletBreakCooldown = builder.comment("How long will you need to wait until you can use Pacifying Amulet when broken (in ticks, 1 sec - 20 ticks) [DEFAULT: 200]").defineInRange("pacifying_amulet_break_cooldown", 200, 0, Integer.MAX_VALUE);
+            pacifyingAmuletAngerOnbreak = builder.comment("Anger all nearby 'angerable'(wolves, golems, etc) entities when pacifying amulet breaks? [DEFAULT: true]").define("pacifying_amulet_anger_onbreak", true);
 
             builder.pop(2);
         }
@@ -116,27 +116,27 @@ public class ModConfig {
     }
 
     public static class Client {
-        private final ForgeConfigSpec.BooleanValue USE_LATIN_ELEMENT_NAMES;
-        private final ForgeConfigSpec.BooleanValue AMULETS_TIER_DIFFERENCE;
-        private final ForgeConfigSpec.BooleanValue RENDER_COMBINATOR_STACK;
-        private final ForgeConfigSpec.BooleanValue RENDER_LEAF_SHIELD;
-        private final ForgeConfigSpec.BooleanValue RENDER_LEAF_CHARGE_OVERLAY;
-        private final ForgeConfigSpec.BooleanValue SHOW_SPLASHES;
+        private final ForgeConfigSpec.BooleanValue useLatinElementNames;
+        private final ForgeConfigSpec.BooleanValue amuletsTierDifference;
+        private final ForgeConfigSpec.BooleanValue renderCombinatorStack;
+        private final ForgeConfigSpec.BooleanValue renderLeafShield;
+        private final ForgeConfigSpec.BooleanValue renderLeafChargeOverlay;
+        private final ForgeConfigSpec.BooleanValue showSplashes;
 
         public Client(ForgeConfigSpec.Builder builder) {
-            SHOW_SPLASHES = builder.comment("Show custom splash(es)? [DEFAULT: true]").translation("config.elementalamulets.show_splashes").define("show_splashes", true);
-            USE_LATIN_ELEMENT_NAMES = builder.comment("Use latin names for the elements(Ignis, Aer, Aqua, Terra and Aether) [DEFAULT: false]").translation("config.elementalamulets.elements_latin_variant").define("elements_latin_variant", false);
+            showSplashes = builder.comment("Show custom splash(es)? [DEFAULT: true]").translation("config.elementalamulets.show_splashes").define("show_splashes", true);
+            useLatinElementNames = builder.comment("Use latin names for the elements(Ignis, Aer, Aqua, Terra and Aether) [DEFAULT: false]").translation("config.elementalamulets.elements_latin_variant").define("elements_latin_variant", false);
             builder.push("Rendering");
-            RENDER_COMBINATOR_STACK = builder.comment("Render output slot on the top Elemental Combinator? [DEFAULT: true]").translation("config.elementalamulets.elemental_combinator_stack_display").define("render_elemental_combinator_stack", true);
+            renderCombinatorStack = builder.comment("Render output slot on the top Elemental Combinator? [DEFAULT: true]").translation("config.elementalamulets.elemental_combinator_stack_display").define("render_elemental_combinator_stack", true);
 
             builder.push("Amulets");
 
             builder.push("Amulet of Terra-Protection");
-            RENDER_LEAF_CHARGE_OVERLAY = builder.comment("Render a leaf charge overlay? [DEFAULT: true]").translation("config.elementalamulets.render_leaf_charge_overlay").define("render_leaf_charge_overlay", true);
-            RENDER_LEAF_SHIELD = builder.comment("Render a leaf shield around the player? [DEFAULT: true]").translation("config.elementalamulets.render_leaf_shield").define("render_leaf_shield", true);
+            renderLeafChargeOverlay = builder.comment("Render a leaf charge overlay? [DEFAULT: true]").translation("config.elementalamulets.render_leaf_charge_overlay").define("render_leaf_charge_overlay", true);
+            renderLeafShield = builder.comment("Render a leaf shield around the player? [DEFAULT: true]").translation("config.elementalamulets.render_leaf_shield").define("render_leaf_shield", true);
             builder.pop();
 
-            AMULETS_TIER_DIFFERENCE = builder.comment("Set to true, if you want amulets to be different based on their tier [DEFAULT: true]").translation("config.elementalamulets.amulets_tier_difference").define("amulets_tier_difference", true);
+            amuletsTierDifference = builder.comment("Set to true, if you want amulets to be different based on their tier [DEFAULT: true]").translation("config.elementalamulets.amulets_tier_difference").define("amulets_tier_difference", true);
             builder.pop();
 
             builder.pop();
@@ -153,15 +153,15 @@ public class ModConfig {
     }
 
     public static class Common {
-        private final ForgeConfigSpec.BooleanValue GENERATE_CULT_TEMPLE;
-        private final ForgeConfigSpec.BooleanValue GENERATE_JEWELLER_HOUSE;
-        private final ForgeConfigSpec.BooleanValue GENERATE_ORES;
+        private final ForgeConfigSpec.BooleanValue generateCultTemple;
+        private final ForgeConfigSpec.BooleanValue generateJewellerHouse;
+        private final ForgeConfigSpec.BooleanValue generateOres;
 
         public Common(ForgeConfigSpec.Builder builder) {
             builder.push("World generation");
-            GENERATE_CULT_TEMPLE = builder.comment("Generate Cult's temple ruins? [DEFAULT: true, requires world restart]").translation("config.elementalamulets.generate_cult_temple").worldRestart().define("generate_cult_temple", true);
-            GENERATE_JEWELLER_HOUSE = builder.comment("Generate Jeweller's house? [DEFAULT: true, requires world restart]").translation("config.elementalamulets.generate_jeweller_house").worldRestart().define("generate_jeweller_house", true);
-            GENERATE_ORES = builder.comment("Generate Elemental Shards ore? [DEFAULT: true, requires world restart]").translation("config.elementalamulets.generate_ores").worldRestart().define("generate_ores", true);
+            generateCultTemple = builder.comment("Generate Cult's temple ruins? [DEFAULT: true, requires world restart]").translation("config.elementalamulets.generate_cult_temple").worldRestart().define("generate_cult_temple", true);
+            generateJewellerHouse = builder.comment("Generate Jeweller's house? [DEFAULT: true, requires world restart]").translation("config.elementalamulets.generate_jeweller_house").worldRestart().define("generate_jeweller_house", true);
+            generateOres = builder.comment("Generate Elemental Shards ore? [DEFAULT: true, requires world restart]").translation("config.elementalamulets.generate_ores").worldRestart().define("generate_ores", true);
             builder.pop();
         }
     }
@@ -189,20 +189,20 @@ public class ModConfig {
         public static boolean MODIFY_VANILLA_LOOT;
 
         private static void cacheServerConfig() {
-            FANCY_COMBINATION = SERVER.FANCY_COMBINATION.get();
-            OLD_FASHIONED_WAY = SERVER.OLD_FASHIONED_WAY.get();
-            JUMP_AMULET_BOOST = SERVER.JUMP_AMULET_BOOST.get();
-            FIRE_AMULET_FIRE_RESISTANCE = SERVER.FIRE_AMULET_FIRE_RESISTANCE.get();
-            FIRE_AMULET_LAVA_RESISTANCE = SERVER.FIRE_AMULET_LAVA_RESISTANCE.get();
-            SPEED_AMULET_BOOST = SERVER.SPEED_AMULET_BOOST.get();
-            PROTECTION_AMULET_REFLECT_DAMAGE_MULT = SERVER.PROTECTION_AMULET_REFLECT_DAMAGE_MULT.get();
-            PROTECTION_AMULET_CHARGE_TIME = SERVER.PROTECTION_AMULET_CHARGE_TIME.get();
-            WATER_AMULET_SPEED_BOOST = SERVER.WATER_AMULET_SPEED_BOOST.get();
-            EARTH_AMULET_COOLDOWN = SERVER.EARTH_AMULET_COOLDOWN.get();
-            MODIFY_VANILLA_LOOT = SERVER.MODIFY_VANILLA_LOOT.get();
-            PACIFYING_AMULET_BREAK_COOLDOWN = SERVER.PACIFYING_AMULET_BREAK_COOLDOWN.get();
-            PACIFYING_AMULET_ANGER_ONBREAK = SERVER.PACIFYING_AMULET_ANGER_ONBREAK.get();
-            PACIFYING_AMULET_DISORIENTATION_TIME = SERVER.PACIFYING_AMULET_DISORIENTATION_TIME.get();
+            FANCY_COMBINATION = SERVER.fancyCombination.get();
+            OLD_FASHIONED_WAY = SERVER.oldFashionedWay.get();
+            JUMP_AMULET_BOOST = SERVER.jumpAmuletBoost.get();
+            FIRE_AMULET_FIRE_RESISTANCE = SERVER.fireAmuletFireResistance.get();
+            FIRE_AMULET_LAVA_RESISTANCE = SERVER.fireAmuletLavaResistance.get();
+            SPEED_AMULET_BOOST = SERVER.speedAmuletBoost.get();
+            PROTECTION_AMULET_REFLECT_DAMAGE_MULT = SERVER.protectionAmuletReflectDamageMult.get();
+            PROTECTION_AMULET_CHARGE_TIME = SERVER.protectionAmuletChargeTime.get();
+            WATER_AMULET_SPEED_BOOST = SERVER.waterAmuletSpeedBoost.get();
+            EARTH_AMULET_COOLDOWN = SERVER.earthAmuletCooldown.get();
+            MODIFY_VANILLA_LOOT = SERVER.modifyVanillaLoot.get();
+            PACIFYING_AMULET_BREAK_COOLDOWN = SERVER.pacifyingAmuletBreakCooldown.get();
+            PACIFYING_AMULET_ANGER_ONBREAK = SERVER.pacifyingAmuletAngerOnbreak.get();
+            PACIFYING_AMULET_DISORIENTATION_TIME = SERVER.pacifyingAmuletDisorientationTime.get();
         }
 
         public static boolean AMULETS_TIER_DIFFERENCE;
@@ -213,12 +213,12 @@ public class ModConfig {
         public static boolean SHOW_SPLASHES;
 
         private static void cacheClientConfig() {
-            USE_LATIN_ELEMENT_NAMES = CLIENT.USE_LATIN_ELEMENT_NAMES.get();
-            AMULETS_TIER_DIFFERENCE = CLIENT.AMULETS_TIER_DIFFERENCE.get();
-            RENDER_COMBINATOR_STACK = CLIENT.RENDER_COMBINATOR_STACK.get();
-            RENDER_LEAF_SHIELD = CLIENT.RENDER_LEAF_SHIELD.get();
-            RENDER_LEAF_CHARGE_OVERLAY = CLIENT.RENDER_LEAF_CHARGE_OVERLAY.get();
-            SHOW_SPLASHES = CLIENT.SHOW_SPLASHES.get();
+            USE_LATIN_ELEMENT_NAMES = CLIENT.useLatinElementNames.get();
+            AMULETS_TIER_DIFFERENCE = CLIENT.amuletsTierDifference.get();
+            RENDER_COMBINATOR_STACK = CLIENT.renderCombinatorStack.get();
+            RENDER_LEAF_SHIELD = CLIENT.renderLeafShield.get();
+            RENDER_LEAF_CHARGE_OVERLAY = CLIENT.renderLeafChargeOverlay.get();
+            SHOW_SPLASHES = CLIENT.showSplashes.get();
         }
 
         public static boolean GENERATE_CULT_TEMPLE;
@@ -226,9 +226,9 @@ public class ModConfig {
         public static boolean GENERATE_ORES;
 
         private static void cacheCommonConfig() {
-            GENERATE_CULT_TEMPLE = COMMON.GENERATE_CULT_TEMPLE.get();
-            GENERATE_JEWELLER_HOUSE = COMMON.GENERATE_JEWELLER_HOUSE.get();
-            GENERATE_ORES = COMMON.GENERATE_ORES.get();
+            GENERATE_CULT_TEMPLE = COMMON.generateCultTemple.get();
+            GENERATE_JEWELLER_HOUSE = COMMON.generateJewellerHouse.get();
+            GENERATE_ORES = COMMON.generateOres.get();
         }
     }
 
