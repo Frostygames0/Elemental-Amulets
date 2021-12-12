b53368ecb8549a0035a660fefe66519381d5001a
Ваня <vanya.alekseev6@gmail.com>
2021-12-12 23:14:21 +0300

    Removed commented out code
    Signed-off-by: Ваня <vanya.alekseev6@gmail.com>

--- src/main/java/frostygames0/elementalamulets/util/NBTUtil.java
+++ src/main/java/frostygames0/elementalamulets/util/NBTUtil.java
@@ -33,22 +33,6 @@ public final class NBTUtil {
     public static int getInteger(ItemStack stack, String tagName) {
         return isSafeToGet(stack, tagName) ? stack.getOrCreateTag().getInt(tagName) : 0;
     }
-    /*
-    public static float getFloat(ItemStack stack, String tagName) {
-        return isSafeToGet(stack, tagName) ? stack.getOrCreateTag().getFloat(tagName) : 0;
-    }
-
-    public static double getDouble(ItemStack stack, String tagName) {
-        return isSafeToGet(stack, tagName) ? stack.getOrCreateTag().getDouble(tagName) : 0;
-    }
-
-    public static boolean getBoolean(ItemStack stack, String tagName) {
-        return isSafeToGet(stack, tagName) && stack.getOrCreateTag().getBoolean(tagName);
-    }
-
-    public static String getString(ItemStack stack, String tagName) {
-        return isSafeToGet(stack, tagName) ? stack.getOrCreateTag().getString(tagName) : "";
-    }*/
 
     public static UUID getUUID(ItemStack stack, String tagName) {
         return stack.getOrCreateTag().hasUUID(tagName) ? stack.getOrCreateTag().getUUID(tagName) : new UUID(0L, 0L);
@@ -57,22 +41,6 @@ public final class NBTUtil {
     public static void putInteger(ItemStack stack, String tagName, int value) {
         stack.getOrCreateTag().putInt(tagName, value);
     }
-    /*
-    public static void putDouble(ItemStack stack, String tagName, double value) {
-        stack.getOrCreateTag().putDouble(tagName, value);
-    }
-
-    public static void putFloat(ItemStack stack, String tagName, float value) {
-        stack.getOrCreateTag().putFloat(tagName, value);
-    }
-
-    public static void putBoolean(ItemStack stack, String tagName, boolean value) {
-        stack.getOrCreateTag().putBoolean(tagName, value);
-    }
-
-    public static void putString(ItemStack stack, String tagName, String value) {
-        stack.getOrCreateTag().putString(tagName, value);
-    }*/
 
     public static void putUUID(ItemStack stack, String name, UUID value) {
         stack.getOrCreateTag().putUUID(name, value);
