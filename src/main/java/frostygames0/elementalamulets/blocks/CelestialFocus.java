051b7636936f47f18c3a77275ab735f43efd7518
Ваня <vanya.alekseev6@gmail.com>
2022-01-09 22:31:33 +0300

    Made Celestial Focus waterloggable(I hope right :p)

--- src/main/java/frostygames0/elementalamulets/blocks/CelestialFocus.java
+++ src/main/java/frostygames0/elementalamulets/blocks/CelestialFocus.java
@@ -21,8 +21,16 @@ package frostygames0.elementalamulets.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.IWaterLoggable;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
@@ -31,6 +39,7 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 
 
 import javax.annotation.Nullable;
@@ -41,24 +50,24 @@ import java.util.List;
  * @date 15.09.2021 18:33
  */
 @SuppressWarnings("deprecation")
-public class CelestialFocus extends Block {
+public class CelestialFocus extends Block implements IWaterLoggable {
 
     private static final VoxelShape STAND = VoxelShapes.or(box(0, 0, 0, 2, 16, 2),
             box(0, 0, 14, 2, 16, 16),
             box(14, 0, 0, 16, 16, 2),
             box(14, 0, 14, 16, 16, 16));
-
     private static final VoxelShape STICKS = VoxelShapes.or(box(14, 10, 2, 16, 12, 14),
             box(0, 10, 2, 2, 12, 14),
             box(2, 10, 0, 14, 12, 2),
             box(2, 10, 14, 14, 12, 16));
-
     private static final VoxelShape LENS = box(5, 14.25, 5, 11, 15.25, 11);
-
     public static final VoxelShape CELESTIAL_FOCUS = VoxelShapes.or(STAND, STICKS, LENS);
+
+    private static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
     public CelestialFocus(Properties p_i48440_1_) {
         super(p_i48440_1_);
+        this.registerDefaultState(this.defaultBlockState().setValue(WATERLOGGED, false));
     }
 
     @Override
@@ -70,6 +79,33 @@ public class CelestialFocus extends Block {
     @Override
     public boolean propagatesSkylightDown(BlockState p_200123_1_, IBlockReader p_200123_2_, BlockPos p_200123_3_) {
         return true;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(WATERLOGGED);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext pContext) {
+        BlockPos blockpos = pContext.getClickedPos();
+        FluidState fluidstate = pContext.getLevel().getFluidState(blockpos);
+        return super.getStateForPlacement(pContext).setValue(WATERLOGGED, fluidstate.getType() == Fluids.WATER);
+    }
+
+    @Override
+    public BlockState updateShape(BlockState pState, Direction pFacing, BlockState pFacingState, IWorld pLevel, BlockPos pCurrentPos, BlockPos pFacingPos) {
+        if (pState.getValue(WATERLOGGED)) {
+            pLevel.getLiquidTicks().scheduleTick(pCurrentPos, Fluids.WATER, Fluids.WATER.getTickDelay(pLevel));
+        }
+
+        return super.updateShape(pState, pFacing, pFacingState, pLevel, pCurrentPos, pFacingPos);
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState pState) {
+        return pState.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(pState);
     }
 
     @Override
